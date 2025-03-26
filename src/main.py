1a7a3a0fb96c5915845cb503a5315fdb2f95fd97
박단비 <silverwill39@gmail.com>
2025-03-26 23:05:38 +0900

    [BE] feat #10: DALL·E 3 API 연동 및 일기 이미지 생성 기능 추가
      - 현재 캐릭터 스타일 일관성 미흡 문제 있어 사용하지 않을 가능성이 높음

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from src.claude_api import create_diary
 from src.face_api import router as face_router  # 얼굴 인식 API 추가
+from src.image_api import router as dalle_router  # 그림 생성 API 추가
 
 app = FastAPI()
 
 # API 라우터 등록
 app.include_router(face_router)  # 얼굴 인식 API
+app.include_router(dalle_router)  # 그림 생성 API
 
 
 @app.get("/")
