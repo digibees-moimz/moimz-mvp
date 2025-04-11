1ed78f8408b55ab901172add1760f917192d8596
박단비 <silverwill39@gmail.com>
2025-04-11 14:44:52 +0900

    [FE] setting #16: PWA 설치 및 설정 추가

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,19 @@
+import Head from "next/head";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#10b981" />
+        <meta name="application-name" content="Moimz" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
